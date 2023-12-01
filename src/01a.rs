65fe6acfe1dfafb0c47fd00d2e917d579adf5cb9
Mateusz Kwapich <mateusz@kwapich.it>
2023-12-01 09:54:22 +0000

    initial commit

---
+++ src/01a.rs
@@ -0,0 +1,26 @@
+// Advent of Code 2022
+// (c) 2002 Mateusz Kwapich
+
+use anyhow::Result;
+use std::io::BufRead;
+
+fn main() -> Result<()> {
+    let stdin = std::io::stdin();
+    let mut sum = 0;
+    for line in stdin.lock().lines() {
+        let line = line?;
+        let mut digits = vec![];
+        for c in line.chars() {
+            if let Some(num) = c.to_digit(10) {
+                digits.push(num);
+            }
+        }
+
+        let a = digits.first().unwrap();
+        let b = digits.last().unwrap();
+        let c = a * 10 + b;
+        sum += c;
+    }
+    println!("{sum}");
+    Ok(())
+}
