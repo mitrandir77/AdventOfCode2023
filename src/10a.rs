e768fd56b122ffb1d6cf617df00621feb3367160
Mateusz Kwapich <mateusz@kwapich.it>
2023-12-09 05:25:35 +0000

    Day 10: scaffolding

---
+++ src/10a.rs
@@ -0,0 +1,37 @@
+// Advent of Code 2023
+// (c) 2023 Mateusz Kwapich
+use anyhow::Result;
+
+use std::io::BufRead;
+
+fn extrapolation(mut seq: Vec<i64>) -> Vec<i64> {
+    if seq.iter().all(|val| *val == 0)  {
+        seq.push(0);
+        seq
+    } else {
+        let diff = seq.iter().skip(1).zip(seq.iter()).map(|(a, b)| a - b).collect();
+        let new_diff = extrapolation(diff);
+        seq.push(seq.last().unwrap() + new_diff.last().unwrap());
+        seq
+    }
+}
+
+
+fn main() -> Result<()> {
+    let stdin = std::io::stdin();
+
+    let mut sum = 0;
+    for line in stdin.lock().lines() {
+        let line = line?;
+        let seq: Vec<i64> = line
+            .split_whitespace()
+            .map(|n| n.parse().unwrap())
+            .rev()
+            .collect();
+        let extrapolated = extrapolation(seq);
+        sum += extrapolated.last().unwrap();
+    }
+
+    println!("{sum}");
+    Ok(())
+}
